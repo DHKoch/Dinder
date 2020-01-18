84bb07986e3cf248422aecf0f5ac55ea90a7e42b
DHKoch <dhktg2@mail.missouri.edu>
2020-01-17 22:00:31 -0600

    Initial Commit

---
+++ DinderTests/DinderTests.swift
@@ -0,0 +1,34 @@
+//
+//  DinderTests.swift
+//  DinderTests
+//
+//  Created by Dalton Koch on 1/17/20.
+//  Copyright © 2020 Dalton Koch. All rights reserved.
+//
+
+import XCTest
+@testable import Dinder
+
+class DinderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
